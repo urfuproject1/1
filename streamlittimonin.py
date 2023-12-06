c13c3132cc7aa062ce2905eb0b2018a19b7a9777
ivan <team-on-in@mail.ru>
2023-12-07 02:18:57 +0300

    Added streamlittimonin.py

--- streamlittimonin.py
+++ streamlittimonin.py
@@ -6,13 +6,20 @@ from PIL import Image
 
 
 def load_image():
+   #Cоздание формы для загрузки изображения
+   #Форма для загрузки изображения средствами стримлит
   uploaded_file = st.file_uploader(
       label='выберите изображение для распознавания')
   if uploaded_file is not None:
+   # получение загруженного изображения
       image_data = uploaded_file.getvalue()
+   #показ загруженного изображения
       st.image(image_data)
+   # возврат изображения в формате пил
       return Image.open(io.BytesIO(image_data))
   else: 
       return None
+   # выводим заголовок
 st.title('Классификация изображений')
-img = load_image() # ['Самолет на взлетно-посадочной полосе аэропорта.']
+  # вызываем функциюю создания формы загрузки изображения
+img = load_image() 
