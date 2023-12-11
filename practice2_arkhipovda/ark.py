501d2be0074ed39f7baabc0185abd5ac1f5d5d1e
Danil Arkhipov <arkhipov.danil2015@gmail.com>
2023-12-11 21:40:54 +0300

    change model

--- practice2_arkhipovda/ark.py
+++ practice2_arkhipovda/ark.py
@@ -4,7 +4,7 @@ from transformers import BlipProcessor, BlipForConditionalGeneration
 
 # Инициализация BlipProcessor и BlipForConditionalGeneration моделей
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
-model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base").to("cuda")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 
 def main():
     # Заголовок веб-приложения
